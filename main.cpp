0ac4aa7124db18216636b1a967667d29722059a9
Miash <filitovme@gmail.com>
2024-01-16 10:49:18 +0000

    added mult

--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@
 
 #include "mylib.h"
 #include "pretty_printer.h"
+#include "mult.h"
 
 int main() {
 
 	int num = give_num();
 	print(num);
+
+	int num_2 = give_num();
+	int mult_res = mult(num_2, num);
+	print(mult_res);
 
 }
---
+++ mult.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+int mult(int a, int b) {
+
+	return a * b;
+}
